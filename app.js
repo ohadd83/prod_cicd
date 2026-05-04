3e70b7e8661ab3698d29dec6f4b0a2cf75b1e7f3
root <root@ip-172-31-30-249.eu-central-1.compute.internal>
2026-05-04 08:03:00 +0000

    change app text

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end("Hello Jenkins CI/CD lets move on aaaaaa bbbbb ccccc");
+  res.end("Hello Jenkins CI/CD lets move on aaaaaa bbbbb ccccc 4_5_2026");
 }).listen(3000);
 
